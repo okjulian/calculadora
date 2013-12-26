eb197faa2914f5220bb9d228dd3a6134ebcce66f
El Juli <mayorga.julian@gmail.com>
2013-12-26 10:29:19 -0300

    feat(inicial): crear esqueleto de app angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('calculadoraApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
